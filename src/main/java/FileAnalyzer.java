093af69758f9ce098636dfd41b441a76509dfec6
Pav.Rychko <Pav.rychko@gmail.com>
2021-11-29 00:10:23 +0200

    File analyzer almost done

--- src/main/java/FileAnalyzer.java
+++ src/main/java/FileAnalyzer.java
@@ -1,26 +1,52 @@
 import java.io.*;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 
 public class FileAnalyzer {
+    File file;
     int repeats;
     String[] sentences;
 
-
-    public int wordRepeatCounts(String path, String word) throws IOException {
-        File file = new File(path);
-        if (!file.isFile()) {
-            throw new FileNotFoundException("path " + path + " is not File path!");
+    FileAnalyzer(String pathToFile) {
+        this.file = new File(pathToFile);
+        try {
+            checkIsFileExist(file, pathToFile);
+        } catch (IOException e) {
+            System.out.print("path " + file.getPath() + " is not File path!");
+            file = null;
         }
-        InputStream inputStream = new FileInputStream(file);
-        String fileText = new String(inputStream.readAllBytes());
-        inputStream.close();
-        char[] textInChars = fileText.toLowerCase().toCharArray();
-        char[] wordInChars = word.toLowerCase().toCharArray();
-        return this.repeats = containsCount(wordInChars, textInChars);
+    }
+
+    public static void main(String[] args) throws IOException {
+
     }
 
 
-    public String[] getSentencesWithWord(String word) {
+    public int wordRepeatCounts(String path, String word) throws IOException {
+        String fileText = readFile(file = new File(path), path);
+        if (isContains(fileText, word)) {
+            char[] textInChars = fileText.toLowerCase().toCharArray();
+            char[] wordInChars = word.toLowerCase().toCharArray();
+            return this.repeats = containsCount(wordInChars, textInChars);
+        }
+        return 0;
+    }
+
+
+    public String[] getSentencesWithWord(String path, String word) throws IOException {
+        String fileText = readFile(file = new File(path), path);
+        if (isContains(fileText, word)) {
+          String[] sentences = fileText.split(",!");
+            ArrayList<String> arrayList = new ArrayList<>();
+            for (int i = 0; i < sentences.length; i++) {
+                if(sentences[i].contains(word)){
+                    arrayList.add(sentences[i]);
+                }
+            }
+            return arrayList.toArray(String[]::new);
+        }
+
         return null;
     }
 
@@ -31,20 +57,54 @@ public class FileAnalyzer {
         for (int i = 0; i < text.length; i++) {
             if (text[i] == word[0]) {
                 sequence = 0;
-                for (int j = 0; j < word.length-1; j++) {
+                for (int j = 0; j < word.length - 1; j++) {
                     if (i + j > text.length) {
                         break;
-                    } else if (text[i+j] == word[j] && text[i+j+1] == word[j+1] ) {
+                    } else if (text[i + j] == word[j] && text[i + j + 1] == word[j + 1]) {
                         sequence++;
                     }
                 }
-                if (sequence == word.length-1) {
+                if (sequence == word.length - 1) {
                     count++;
                 }
 
             }
         }
         return count;
+    }
+
+    private boolean isContains(String text, String word) {
+        return text.toLowerCase().contains(word.toLowerCase());
+    }
+
+    private String readFile(File file, String path) throws IOException {
+        checkIsFileExist(file, path);
+        InputStream inputStream = new FileInputStream(file);
+        StringBuilder stringBuilder = new StringBuilder();
+        String sentence;
+        byte[] buffer = new byte[1024];
+        int b;
+        while ((b =inputStream.read()) != -1){
+            for (int i = 0; i < 1024; i++) {
+                b = inputStream.read();
+                buffer[i] = (byte)b;
+                if ((b == 33 || b == 63 || b == 46)) {
+                    sentence = new String(buffer, 0, i, "UTF-8");
+                    stringBuilder.append(sentence);
+                    stringBuilder.append(",!"); // problem!
+                    i = 0;
+                }
+            }
+        }
+        inputStream.close();
+        return stringBuilder.toString();
+    }
+
+
+    private void checkIsFileExist(File file, String path) throws IOException {
+        if (!file.isFile()) {
+            throw new FileNotFoundException("path " + path + " is not File path!");
+        }
     }
 }
 
--- src/test/java/FileAnalyzerTest.java
+++ src/test/java/FileAnalyzerTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -6,25 +8,40 @@ import java.io.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FileAnalyzerTest {
-    FileAnalyzer fileAnalyzer = new FileAnalyzer();
+    File file = new File("story.txt");
+    FileAnalyzer fileAnalyzer = new FileAnalyzer(file.getPath());
+
+    @BeforeEach
+    private void createFile() throws IOException {
+        if (file.exists()) {
+            deleteFile();
+        }
+        file.createNewFile();
+        assertEquals("story.txt", file.getPath());
+        assertTrue(file.exists());
+        OutputStream outputStream = new FileOutputStream("story.txt");
+        byte[] storyBytes = getStoryText().getBytes();
+        outputStream.write(storyBytes);
+        outputStream.close();
+    }
+
 
     @DisplayName("how many times word is repeated in a text test")
     @Test
     public void wordsRepeatCountTest() throws IOException {
-        File file = createFile();
-        String actual = assertThrows(FileNotFoundException.class, ()-> fileAnalyzer.wordRepeatCounts("c:/jdk-17", "заяц")).getMessage();
+
+        String actual = assertThrows(FileNotFoundException.class, () -> fileAnalyzer.wordRepeatCounts("c:/jdk-17", "заяц")).getMessage();
         String expected = "path c:/jdk-17 is not File path!";
         assertEquals(expected, actual);
         assertEquals(10, fileAnalyzer.wordRepeatCounts(file.getPath(), "Заяц"));
         assertEquals(0, fileAnalyzer.wordRepeatCounts(file.getPath(), "Волк"));
-        assertTrue(file.delete());
 
-}
 
-@DisplayName("Get and print all sentences that have your word test")
-@Test
-public void sentencesThatContainWordTest() throws IOException {
-        File file = createFile();
+    }
+
+    @DisplayName("Get and print all sentences that have your word test")
+    @Test
+    public void sentencesThatContainWordTest() throws IOException {
         String[] expected = {
                 "Наверное, птенец, но тень от его крыльев город закрывает, в городе ночь настаёт.",
                 "Город, небось, крохотный?",
@@ -34,30 +51,18 @@ public void sentencesThatContainWordTest() throws IOException {
                 "Чего-чего, на хромой блохе с того берега моря, которое зайцу не перелететь, орлу не перебежать, хоть море не море, а так, лужа посреди города," +
                         " где тень от блохи на зайца упала и насмерть убила, а из шкуры зайца тулуп вышел и пошёл куда глаза глядят, а тут заяц ка-ак прыгнет!"
         };
-        assertEquals(expected, fileAnalyzer.getSentencesWithWord("город"));
-        assertTrue(file.delete());
+        assertEquals(expected[0], fileAnalyzer.getSentencesWithWord("story.txt","город")[0]);
+        assertEquals(expected[1], fileAnalyzer.getSentencesWithWord("story.txt","город")[1]);
+        assertEquals(expected[2], fileAnalyzer.getSentencesWithWord("story.txt","город")[2]);
+        assertEquals(expected[3], fileAnalyzer.getSentencesWithWord("story.txt","город")[3]);
+        assertEquals(expected[4], fileAnalyzer.getSentencesWithWord("story.txt","город")[4]);
+        assertEquals(expected[5], fileAnalyzer.getSentencesWithWord("story.txt","город")[5]);
 
 
-}
-
-private File createFile() throws IOException {
-    File file = new File("story.txt");
-    assertFalse(file.exists());
-    assertTrue(file.createNewFile());
-    assertEquals("story.txt", file.getPath());
-    assertTrue( file.exists());
-    OutputStream outputStream = new FileOutputStream("story.txt");
-    byte[] storyBytes = getStoryText().getBytes();
-    outputStream.write(storyBytes);
-    outputStream.close();
-    return file;
-}
+    }
 
 
-
-
-
-private String getStoryText(){
+    private String getStoryText() {
         return "Это я, добрый Э-эх, я здесь." +
                 "\n" +
                 "И я здесь!" +
@@ -105,5 +110,10 @@ private String getStoryText(){
                 "Какой заяц, какой орёл, какая блоха?!" +
                 "\n" +
                 "Так повторить? Ну, значит, та самая блоха с того берега лужи…";
-}
+    }
+
+    @AfterEach
+    private void deleteFile() {
+        file.delete();
+    }
 }
